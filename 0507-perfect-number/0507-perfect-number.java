0cb1adc1debc7354c54a5760bbf699f8ed386078
Prakash Jena <144409335+prakashjena938@users.noreply.github.com>
2026-07-28 11:19:07 +0530

    Time: 2133 ms (5.38%), Space: 42.3 MB (10.1%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    int sum;
+    public boolean checkPerfectNumber(int num) {
+
+        
+        for(int i = 1; i<=num/2;i++){
+            if(num % i == 0)  sum = sum+i; 
+            
+
+        }
+        if(sum == num ) return true;
+
+        return false; 
+        
+    }
+}
