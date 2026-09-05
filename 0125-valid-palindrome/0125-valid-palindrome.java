343512c3e05fe559f670b93c83a2c24b2c72c399
Prakash Jena <144409335+prakashjena938@users.noreply.github.com>
2026-09-05 23:47:04 +0530

    Time: 2 ms (99.31%), Space: 44.4 MB (53.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,47 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-
+        int left =0;
+        int right = s.length()-1;
+        s = s.toLowerCase();
+        while(right>left){
             char l = s.charAt(left);
             char r = s.charAt(right);
-
-            // Skip non-alphanumeric from left
-            if (!((l >= 'A' && l <= 'Z') ||
-                  (l >= 'a' && l <= 'z') ||
-                  (l >= '0' && l <= '9'))) {
+            if(!((l>='a'&& l<='z')||(l >= '0' && l <= '9'))) {
                 left++;
                 continue;
             }
-
-            // Skip non-alphanumeric from right
-            if (!((r >= 'A' && r <= 'Z') ||
-                  (r >= 'a' && r <= 'z') ||
-                  (r >= '0' && r <= '9'))) {
+            if(!((r>='a'&& r<='z')||(r >= '0' && r <= '9'))) {
                 right--;
                 continue;
             }
-
-            // Convert uppercase to lowercase
-            if (l >= 'A' && l <= 'Z') {
-                l = (char)(l + 32);
-            }
-
-            if (r >= 'A' && r <= 'Z') {
-                r = (char)(r + 32);
-            }
-
-            if (l != r) {
+            if(!(l==r)){
                 return false;
             }
-
-            left++;
             right--;
+            left++;
         }
-
         return true;
     }
 }
