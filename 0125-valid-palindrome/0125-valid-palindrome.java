ea8323913d7b7bcf4448bc23b11e3c87ccd9493c
Prakash Jena <144409335+prakashjena938@users.noreply.github.com>
2026-09-05 23:00:53 +0530

    Time: 250 ms (5.18%), Space: 53.5 MB (5.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -5,12 +5,15 @@ class Solution {
         s=s.toLowerCase();
         for(int i = s.length()-1; i>=0;i--){
             char f = s.charAt(i);
-            if((f>='a'&& f<='z')||(f>='0'&& f<='9'))  rev+=f;
+            if((f>='a'&& f<='z')||(f>='0'&& f<='9')){  
+                res=f+res;
+                rev=rev+f;
+            }
         }
-        for(int j = 0; j<s.length();j++){
-            char b = s.charAt(j);
-            if((b>='a'&& b<='z')||(b>='0'&& b<='9'))  res+=b;
-        }
+        // for(int j = 0; j<s.length();j++){
+        //     char b = s.charAt(j);
+        //     if((b>='a'&& b<='z')||(b>='0'&& b<='9'))  res+=b;
+        // }
         if(rev.equals(res)) return true;
 
         return false;
