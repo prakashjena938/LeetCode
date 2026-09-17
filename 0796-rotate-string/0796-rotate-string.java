6deb681380bb1b3d3c825b6d777cbd9bc796d68a
Prakash Jena <144409335+prakashjena938@users.noreply.github.com>
2026-09-17 12:31:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         if(goal.length()<s.length() ) return false;
         s = s+s;
-        if(s.contains(goal)) return true;
+        else(s.contains(goal)) return true;
         return false;
     }
 }
